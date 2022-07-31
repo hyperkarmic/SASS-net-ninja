d99aca260511a63c566b9e195467b2568b14e259
hyperkarmic <randy_pan@yahoo.com>
2022-07-31 12:15:56 +0100

    the _ symbol turns scss file into partials ie not processed by gulp. * is a wildcard for variables in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("index.scss").pipe(sass()).pipe(dest("css"));
+  return src("*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["index.scsss"], buildStyles);
+  watch(["*.scsss"], buildStyles);
 }
 exports.default = series(buildStyles, watchTask);
